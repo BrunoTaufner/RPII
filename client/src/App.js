0936d1932a6437b7d9efb55dc1de360ab197ca83
GustavoAkashi <gustavoakashi@hotmail.com>
2022-10-26 09:34:34 -0300

    Adding routes and Catalog page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
-import Header from './components/Header/Header.js'
+import React from 'react';
+import Routes from './routes'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Qato Doações</h1>
-      <Header> </Header>
-    </div>
+    <>
+      <h1>Hello</h1>
+      <Routes />
+    </>
+
   );
 }
+
 export default App;
