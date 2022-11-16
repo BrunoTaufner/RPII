3e31ed4440376b5d7b82587cc84fb99ed96e34ab
feelimelek <80131744+feelimelek@users.noreply.github.com>
2022-11-16 11:48:03 -0300

    Pagina Login ok, qunado clica no link CLIQUE AQUI muda a tela para fazer o cadastro

--- client/src/components/TopFormulario/TopFormulario.js
+++ client/src/components/TopFormulario/TopFormulario.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import styles from './TopFormulario.module.css';
-import Label_Cadastre_Se from '../labels/Label_Cadastre_Se/Label_Cadastre_Se';
+import LabelTitulo from '../labels/LabelTitulo/LabelTitulo';
 import Label_Tipo_Cadastro from '../labels/Label_Tipo_Cadastro/Label_Tipo_Cadastro';
 import Label_Numero_Pergunta from '../labels/Label_Numero_Pergunta/Label_Numero_Pergunta';
 import Label_Pergunta from '../labels/Label_Pergunta/Label_Pergunta';
 
 const TopFormulario = () => (
   <div>
-    <Label_Cadastre_Se />
+    <LabelTitulo texto={"Cadastre-se"}/>
     {/* TODO colocar uma variavel no tipo cadastro */}
     <Label_Tipo_Cadastro tipoCadastro={"tipoCadastro"}/>
     <Label_Numero_Pergunta numeradorPergunta={"1"} denominadorPergunta={"7"}/>
