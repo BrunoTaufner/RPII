fa17f60f21f2c73d73fe705af65223c31235d5f7
vinimarianob <vmariano2011@gmail.com>
2022-11-16 11:32:05 -0300

    Dev da página Home

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,13 +1,21 @@
 import React from "react";
-
+import ong_animais from '../assets/img/ong-animais-abandonados.jpg'
+import {Title, Box, BoxText} from '../styles/Styles';
 const Home = () => {
-return (
-	<div>
-	<h1>
-		Qato
-	</h1>
-	</div>
-);
+	return (
+		<Box>
+			<Title>Principais causas da semana</Title>
+			<img src={ong_animais} width="30%"></img>
+			<BoxText>
+				Lorem ipsum dolor sit amet, consectetur adipisci elit,
+				sed eiusmod tempor incidunt ut labore et dolore magna aliqua.
+				Ut enim ad minim veniam, quis nostrum exercitationem ullam
+				corporis suscipit laboriosam, nisi ut aliquid ex ea commodi
+				consequatur. Quis aute iure reprehenderit in voluptate velit
+				esse cillum dolore eu fugiat nulla pariatur.
+			</BoxText>
+		</Box>
+	);
 };
 
 export default Home;
--- client/src/styles/Styles.js
+++ client/src/styles/Styles.js
@@ -1,13 +1,13 @@
 import { FaBars } from 'react-icons/fa';
 import styled from 'styled-components';
 export const Title = styled.h1`
-font-size: 70px;
+font-size: 60px;
 font-weight: 600;
 background-image: linear-gradient(to left, #553c9a, #b393d3);
 color: transparent;
 background-clip: text;
 -webkit-background-clip: text;
-  text-align: center;
+text-align: center;
   @font-face {
     font-family: 'Helvetica';
     src: local(''), url(fonts/helvetica-rounded-bold.otf) format('otf');
@@ -31,3 +31,11 @@ export const Input = styled.input.attrs(props => ({
     left: 50%;
     transform: translate(-50%, -50%);	
   `;
+
+export const Box = styled.div `
+  margin-left:10%;
+  `;
+
+export const BoxText = styled.text `
+margin-left:10%;
+`;
